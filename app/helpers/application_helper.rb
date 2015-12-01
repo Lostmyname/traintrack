6a85e478de0d1fdef7f8ef8a5e4440041035ad83
Callum Macrae <callum@macr.ae>
2015-12-01 16:46:53 +0000

    currency formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def format_currency(amount)
+    args = { unit: '£' }
+
+    if amount % 1 == 0
+      args[:precision] = 0;
+    end
+
+    number_to_currency(amount, args)
+  end
 end
