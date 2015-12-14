83a7580682920da9f8bd5524490f3d7b54d2f627
Callum Macrae <callum@macr.ae>
2015-12-14 10:39:39 +0000

    Removed /people/new, becasue oauth

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,7 @@ class PeopleController < ApplicationController
   include ApplicationHelper
 
   before_action :authenticate_person!
-  before_action :authenticate_admin!, only: [:new, :create, :update]
+  before_action :authenticate_admin!, only: [:update]
 
   # List people.
   def index
@@ -14,23 +14,6 @@ class PeopleController < ApplicationController
   def show
     @person = get_person_from_params
     @expense = Expense.new
-  end
-
-  # Form to create a person. Admin only.
-  def new
-    @person = Person.new
-  end
-
-  # Create a person. Admin only.
-  def create
-    @person = Person.new(person_params)
-    @person.update_attributes(remaining: @person.budget)
-
-    if @person.save
-      redirect_to @person
-    else
-      render 'new'
-    end
   end
 
   # Change a person (budget / admin status), admin only
@@ -51,10 +34,6 @@ class PeopleController < ApplicationController
   end
 
   private
-
-  def person_params
-    params.require(:people).permit(:name, :budget, :admin)
-  end
 
   def get_person_from_params
     Person.find(params[:id] == 'me' ? current_person.id : params[:id])
