ea3ab6bddb6c080a3d30ff7cfb6dfcffe44bc739
Callum Macrae <callum@macr.ae>
2015-12-15 14:23:12 +0000

    Fixed bug where redirected to incorrect place on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || people_path
+    session[:previous_url] || root_path
   end
 end
